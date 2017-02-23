ccba8e42d1c5cbbc1263b8519787444ccd7ea127
Nilam Dahal <Nilam Dahal>
2017-02-22 23:30:25 -0600

    initial push

---
+++ src/main/java/com/nilam/main/EmsApp.java
@@ -0,0 +1,12 @@
+package com.nilam.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApp {
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApp.class, args);
+	}
+
+}
