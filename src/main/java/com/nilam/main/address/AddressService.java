ac550e297554d4fc2159221b20c335d017767dd3
dahalNilam <dahalnilam90@gmail.com>
2017-02-24 14:27:00 -0600

    finilized rest services and added comment

--- src/main/java/com/nilam/main/address/AddressService.java
+++ src/main/java/com/nilam/main/address/AddressService.java
@@ -13,20 +13,24 @@ public class AddressService {
 	@Autowired
 	private AddressRepository addressRepository;
 
+	// Get List of Addresses of Particular Employee
 	public List<Address> getAllAddresses(long id) {
 		List<Address> addresses = new ArrayList<>();
 		addressRepository.findByEmployeeId(id).forEach(addresses::add);
 		return addresses;
 	}
 
+	// Get Address from id
 	public Address getAddress(@PathVariable long id) {
 		return addressRepository.findOne(id);
 	}
 
+	// Save or Update Address
 	public void addAddress(Address address) {
 		addressRepository.save(address);
 	}
 
+	// Delete Address
 	public void deleteAddress(long id) {
 		addressRepository.delete(id);
 	}
--- src/main/java/com/nilam/main/employee/EmployeeService.java
+++ src/main/java/com/nilam/main/employee/EmployeeService.java
@@ -8,24 +8,28 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
+	// Get List of Employees
 	public List<Employee> getAllEmployees() {
 		List<Employee> employeeList = new ArrayList<>();
 		employeeRepository.findAll().forEach(employeeList::add);
 		return employeeList;
 	}
 
+	// Get Employee from id
 	public Employee getEmployee(long id) {
 		return employeeRepository.findOne(id);
 	}
-	
+
+	// Save or Update Employee
 	public void addEmployee(Employee employee) {
 		employeeRepository.save(employee);
 	}
 
+	// Delete Employee
 	public void deleteEmployee(long id) {
 		employeeRepository.delete(id);
 	}
