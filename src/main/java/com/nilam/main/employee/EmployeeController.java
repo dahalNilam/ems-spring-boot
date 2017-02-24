cd2d929ab8c18348136a8cde092064e46c40901b
dahalNilam <dahalnilam90@gmail.com>
2017-02-23 20:07:31 -0600

    added addEmployee method

--- src/main/java/com/nilam/main/employee/EmployeeController.java
+++ src/main/java/com/nilam/main/employee/EmployeeController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,16 +14,20 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	@RequestMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return employeeService.getAllEmployees();
 	}
-	
+
 	@RequestMapping("/employees/{id}")
 	public Employee getEmployee(@PathVariable long id) {
 		return employeeService.getEmployee(id);
 	}
-	
-	
+
+	@RequestMapping(method=RequestMethod.POST, value="/employees")
+	public void addEmployee(@RequestBody Employee employee) {
+		employeeService.addEmployee(employee);
+	}
+
 }
--- src/main/java/com/nilam/main/employee/EmployeeService.java
+++ src/main/java/com/nilam/main/employee/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.nilam.main.employee;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,21 +9,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeService {
 
-	private List<Employee> employees = Arrays.asList(
-			new Employee(1, "Nilam", "Manager", 70000),
-			new Employee(2, "Manish", "CEO", 30000),
-			new Employee(3, "Ashotosh", "Programmer", 40000),
-			new Employee(4, "Gyanu", "Tester", 50000)
-			);
-
 	public List<Employee> getAllEmployees() {
 		return employees;
 	}
-	
+
 	public Employee getEmployee(long id) {
 		return employees.stream()
-		.filter(e -> e.getId() == id)
-		.findFirst()
-		.get();
+				.filter(e -> e.getId() == id)
+				.findFirst()
+				.get();
 	}
+	
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+	}
+
+	private List<Employee> employees = new ArrayList<>(
+			Arrays.asList(
+					new Employee(1, "Nilam", "Manager", 70000),
+					new Employee(2, "Manish", "CEO", 30000),
+					new Employee(3, "Ashotosh", "Programmer", 40000),
+					new Employee(4, "Gyanu", "Tester", 50000)
+					)
+			);
+
 }
